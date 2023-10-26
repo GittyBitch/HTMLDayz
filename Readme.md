cadf4170cb293310737740bbaf3f9d2a6f67de6e
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-10-26 13:36:51 +0200

    add main function to work without uvicorn cli tool

---
+++ Readme.md
@@ -0,0 +1,14 @@
+HTML Übung 3
+
+
+Aufgabe  - Erstelle ein FastAPi Project in deinem Repo lasse die HTML von FastApi rendern
+
+Erstelle eine main.py mit einer GET Route auf Root
+
+Lasse die HTML von fastAPI rendern und ersetze deinen Namen und die Liste der Skills durch Python Variablen.
+
+Du kannst Dich an dieser Anleitung orientieren:
+https://fastapi.tiangolo.com/advanced/templates/
+
+Commit & Push
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+app = FastAPI(title="HTML Dayz", version="0.0.1", openapi_url="/openapi.json")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
@@ -10,5 +10,9 @@ async def read_root(request: Request):
     name = "Boris König"
     skills = ['cooking','begging','sleeping','eating']
     return templates.TemplateResponse("index.html", {"request": request, "name": name, "skills": skills})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
